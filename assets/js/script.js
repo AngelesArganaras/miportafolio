fb8aaa5748af56688e4998ff05e7b0fe399e770d
Angeles <an.arganaras@gmail.com>
2020-05-27 00:32:58 -0300

    smooth

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-lalalala
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var item = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(item).offset().top
+      }, 800, function(){
+        window.location.hash = item;
+      });
+    }
+  });
+});
